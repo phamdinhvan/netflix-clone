3673810e6bbc3d2914345a25c3a818688b7b6e4f
phamdinhvan <pdvan999@gmail.com>
2022-07-07 01:04:32 +0700

    add TMDB and config get API from TMDB and add Banner

--- pages/component/Header.tsx
+++ pages/component/Header.tsx
@@ -1,11 +1,28 @@
-import React from "react";
-import { BellIcon, SearchIcon } from "@heroicons/react/outline";
-import Link from "next/link";
+import React, { useEffect, useState } from 'react';
+import { BellIcon, SearchIcon } from '@heroicons/react/outline';
+import Link from 'next/link';
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 0) {
+      setIsScrolled(true);
+    }
+    setIsScrolled(false);
+  };
+  useEffect(() => {
+    handleScroll();
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <header>
-      <div className="flex items-center space-x-2 md:space-x-10">
+      <div className={`${isScrolled && 'bg-[#141414]'}`}>
         <img
           src="https://rb.gy/ulxxee"
           width={100}
@@ -22,12 +39,16 @@ function Header() {
         </ul>
       </div>
 
-      <div>
+      <div className="flex items-center space-x-4 text-sm">
         <SearchIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
         <Link href="/account">
-          <img src="" alt="" />
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
         </Link>
       </div>
     </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,31 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import { Movie } from '../type';
+import request from '../utils/request';
+import Banner from './component/Banner';
 import Header from './component/Header';
 
-const Home: NextPage = () => {
+interface Props {
+  netflixOriginals: Movie[];
+  trendingNow: Movie[];
+  topRated: Movie[];
+  actionMovies: Movie[];
+  comedyMovies: Movie[];
+  horrorMovies: Movie[];
+  romanceMovies: Movie[];
+  documentaries: Movie[];
+}
+
+const Home: NextPage<Props> = (props) => {
   return (
-    <div className="">
+    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <main>
+        <Banner netflixOriginals={props.netflixOriginals} />
         <section></section>
       </main>
     </div>
@@ -19,3 +33,38 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getServerSideProps = async () => {
+  const [
+    netflixOriginals,
+    trendingNow,
+    topRated,
+    actionMovies,
+    comedyMovies,
+    horrorMovies,
+    romanceMovies,
+    documentaries
+  ] = await Promise.all([
+    fetch(request.fetchNetflixOriginals).then((res) => res.json()),
+    fetch(request.fetchTrending).then((res) => res.json()),
+    fetch(request.fetchTopRated).then((res) => res.json()),
+    fetch(request.fetchActionMovies).then((res) => res.json()),
+    fetch(request.fetchComedyMovies).then((res) => res.json()),
+    fetch(request.fetchHorrorMovies).then((res) => res.json()),
+    fetch(request.fetchRomanceMovies).then((res) => res.json()),
+    fetch(request.fetchDocumentaries).then((res) => res.json())
+  ]);
+
+  return {
+    props: {
+      netflixOriginals: netflixOriginals.results,
+      trendingNow: trendingNow.results,
+      topRated: topRated.results,
+      actionMovies: actionMovies.results,
+      comedyMovies: comedyMovies.results,
+      horrorMovies: horrorMovies.results,
+      romanceMovies: romanceMovies.results,
+      documentaries: documentaries.results
+    }
+  };
+};
